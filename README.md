82f6ba6a8588a1661b32ebecd33c05bc2d6ac62c
hossamrizk <hossamrizk048@gmail.com>
2024-06-10 01:06:23 +0300

    Set GUI Configirations

---
+++ README.md
@@ -0,0 +1,7 @@
+# Chat With Your PDFs
+
+This is an implementation of RAG model for question answering 
+
+## Requirements
+- Python 3.9
+- langchain
---
+++ web_pages/pages.py
@@ -0,0 +1,44 @@
+import streamlit as st
+
+class AppPages:
+
+    def __init__(self):
+        self.pages = {
+            "Home":self.home_page,
+            "Chat!":self.chat_page,
+        }
+
+    def contact_me(self):
+        if st.sidebar.button("Contact me."):
+            st.sidebar.markdown(
+                """
+                **Contact:-**\n
+                *Hossam Eldein Rizk.*\n
+                *AI Engineer.*\n
+                hossamrizk048@gmail.com\n
+                [Linkedin](https://www.linkedin.com/in/hossamrizk10/)\n
+                [Github](https://github.com/hossamrizk)\n
+                [Kaggle](https://www.kaggle.com/hossamrizk)\n
+                """
+            )
+
+    def home_page(self):
+        st.contacy_me()
+        st.title("Welcome to the Chatbot Application")
+        st.write("**Interact with your documents effortlessly**")
+        st.write(
+            "This platform allows you to upload your PDFs and start a conversation with our advanced chatbot about the content. "
+            "Our chatbot is designed to help you extract insights, answer questions, and provide a deeper understanding of your documents."
+        )
+        st.header("Features:")
+        st.write("""
+        - Upload multiple PDFs.
+        - Ask questions about the content of your PDFs.
+        - Receive accurate and context-aware responses.
+        - Easy-to-use interface for seamless interaction.
+        """)
+
+    def chat_page(self):
+        self.contact_me()
+        st.title("Try with your PDFs!")
+    
